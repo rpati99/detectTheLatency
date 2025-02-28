3663683ea56744b4f7a8dae69a617a839edca5c6
Rachit Prajapati <rdprajapati@icloud.com>
2025-02-28 14:36:00 -0800

    worktree clean

--- Sources/callgraphbuilder/HandlerManager/FunctionResolver.swift
+++ Sources/callgraphbuilder/HandlerManager/FunctionResolver.swift
@@ -178,27 +178,5 @@ public class FunctionResolver: SyntaxVisitor {
             )
         }
     }
-    
-    
-    
 }
 
-
-class ReplaceFunctionRewriter: SyntaxRewriter {
-    let targetFunction: String
-    let newFunction: FunctionDeclSyntax
-    
-    init(targetFunction: String, newFunction: FunctionDeclSyntax) {
-        self.targetFunction = targetFunction
-        self.newFunction = newFunction
-        super.init()
-    }
-    
-    override func visit(_ node: FunctionDeclSyntax) -> DeclSyntax {
-        let isStatic = node.modifiers.contains { $0.name.text == "static" }
-        if node.name.text == targetFunction || isStatic {
-            return DeclSyntax(newFunction)
-        }
-        return super.visit(node)
-    }
-}
---
+++ Sources/callgraphbuilder/HandlerManager/ReplaceFunctionRewriter.swift
@@ -0,0 +1,29 @@
+//
+//  ReplaceFunctionRewriter.swift
+//  detectlatency
+//
+//  Created by Rachit Prajapati on 2/28/25.
+//
+
+import SwiftSyntax
+import Foundation
+
+// Replaces old function with new profiled syntax included code.
+class ReplaceFunctionRewriter: SyntaxRewriter {
+    let targetFunction: String
+    let newFunction: FunctionDeclSyntax
+    
+    init(targetFunction: String, newFunction: FunctionDeclSyntax) {
+        self.targetFunction = targetFunction
+        self.newFunction = newFunction
+        super.init()
+    }
+    
+    override func visit(_ node: FunctionDeclSyntax) -> DeclSyntax {
+        let isStatic = node.modifiers.contains { $0.name.text == "static" }
+        if node.name.text == targetFunction || isStatic {
+            return DeclSyntax(newFunction)
+        }
+        return super.visit(node)
+    }
+}
